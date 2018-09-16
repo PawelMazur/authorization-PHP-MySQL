56b4f7fd326fd819eb4773db254a13abf0531d45
PawelMazur <pawelmazur60@gmail.com>
2018-09-16 21:16:21 +0200

    first commit

---
+++ init.php
@@ -0,0 +1,16 @@
+<?php
+
+$host = "localhost";
+$user_name ="1234";
+$user_password = "1234";
+$db_name = "retrofitcrud";
+
+$con = mysqli_connect($host,$user_name,$user_password,$db_name);
+
+// if($con)
+// echo "Connection success...";
+// else
+// echo "Connection Failed";
+
+
+?>
